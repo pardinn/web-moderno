66afbfdfa8e2effb28ddf8f947c1e1a3abf9b9a3
Victor Moraes <dt.victor@gmail.com>
2018-09-15 18:48:02 -0300

    Últimas aulas do capítulo de Orientação a Objetos

---
+++ objeto/classe1.js
@@ -0,0 +1,33 @@
+class Lancamento {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamento('Salario',45000)
+const contaDeLuz = new Lancamento('Luz',-220)
+
+const conta = new CicloFinanceiro(6,2018)
+conta.addLancamentos(salario,contaDeLuz)
+console.log(conta.sumario())
