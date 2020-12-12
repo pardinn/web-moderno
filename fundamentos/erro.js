55d00d9bb2bca2fd5d250347710e364aef66f94b
Victor Moraes <dt.victor@gmail.com>
2020-12-12 16:18:39 -0300

    Melhorando exemplo

--- fundamentos/erro.js
+++ fundamentos/erro.js
@@ -1,24 +1,26 @@
 function tratarErroELancar(erro) {
-    // throw new Error('...')
-    // throw 10
-    // throw true
-    // throw 'mensagem'
-    throw {
-        nome: erro.name,
-        msg: erro.msg,
-        date: new Date
-    }
+  // throw new Error('...')
+  // throw 10
+  // throw true
+  // throw 'mensagem'
+  throw {
+    nome: erro.name,
+    msg: erro.msg,
+    date: new Date
+  }
 }
 
 function imprimirNomeGritado(obj) {
-    try {
-        console.log(obj.name.toUpperCase() + '!!!')
-    } catch (e) {
-        tratarErroELancar(e)
-    } finally {
-        console.log('final')
-    }
+  try {
+    console.log(obj.name.toUpperCase() + '!!!')
+  } catch (e) {
+    tratarErroELancar(e)
+  } finally {
+    console.log('final')
+  }
 }
 
-const obj = { nome: 'Victor' }
+const obj = { name: 'Victor' }
 imprimirNomeGritado(obj)
+const obj2 = { nome: 'Vai dar erro' }
+imprimirNomeGritado(obj2)
