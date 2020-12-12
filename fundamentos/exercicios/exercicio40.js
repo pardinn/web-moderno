d18abc2c528ee06e7abc5152252343c16c5d40aa
Victor Moraes <dt.victor@gmail.com>
2020-12-12 17:06:36 -0300

    Usando prototipo

--- fundamentos/exercicios/exercicio40.js
+++ fundamentos/exercicios/exercicio40.js
@@ -1,16 +1,21 @@
 // Faça uma função que receba como parâmetro um vetor de notas e mostre os conceitos de cada uma de
 // modo que de 0,0 a 4,9 seja atribuído o conceito D, de 5,0 a 6,9 seja atribuído o conceito C, de 7,0 a 8,9 o
 // conceito B e de 9,0 a 10,0 o conceito A
+
+Number.prototype.entre = function (menor, maior) {
+  return this >= menor && this <= maior
+}
+
 function conceituarNotas(notas) {
   let conceitos = [];
   notas.forEach((nota) => {
-    if (nota >= 0 && nota <= 4.9) {
+    if (nota.entre(0, 4.9)) {
       conceitos.push({ nota: nota, conceito: 'D' });
-    } else if (nota >= 5.0 && nota <= 6.9) {
+    } else if (nota.entre(5.0, 6.9)) {
       conceitos.push({ nota: nota, conceito: 'C' });
-    } else if (nota >= 7.0 && nota <= 8.9) {
+    } else if (nota.entre(7.0, 8.9)) {
       conceitos.push({ nota: nota, conceito: 'B' });
-    } else if (nota >= 9.0 && nota <= 10.0) {
+    } else if (nota.entre(9.0, 10.0)) {
       conceitos.push({ nota: nota, conceito: 'A' });
     } else {
       conceitos.push({ nota: nota, conteito: 'Nota inválida' });
