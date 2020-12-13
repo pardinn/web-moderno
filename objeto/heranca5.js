41cb7fbdd31b07c8b2375f5fe1f4a87c29be419c
Victor Moraes <dt.victor@gmail.com>
2020-12-13 16:33:38 -0300

    Correção do exercício

--- objeto/heranca5.js
+++ objeto/heranca5.js
@@ -3,20 +3,20 @@ console.log(typeof Array)
 console.log(typeof Object)
 
 String.prototype.reverse = function () {
-    return this.split('').reverse().join('')
+  return this.split('').reverse().join('');
 }
 
-console.log('Escola Cod3r'.reverse())
+console.log('Escola Cod3r'.reverse());
 
 Array.prototype.first = function () {
-    return this[0]
+  return this[0];
 }
 
-console.log([1, 2, 3, 4, 5].first())
-console.log(['a', 'b', 'c'].first())
+console.log([1, 2, 3, 4, 5].first());
+console.log(['a', 'b', 'c'].first());
 
 String.prototype.toString = function () {
-    console.log('Lascou tudo')
+  return 'Lascou tudo';
 }
 
-console.log('Escola Cod3r'.reverse())
+console.log('Escola Cod3r'.reverse());
