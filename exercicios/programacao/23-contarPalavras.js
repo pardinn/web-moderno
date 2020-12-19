387a2262111abcb04540aa11c5e9e977f1ea01d2
Victor Moraes <dt.victor@gmail.com>
2020-12-19 15:06:51 -0300

    Resolução do exercício 23

---
+++ exercicios/programacao/23-contarPalavras.js
@@ -0,0 +1,10 @@
+// Criar uma função que receba uma string como parâmetro e conte quantas palavras tem nela.
+// 📕 Considere que todas as palavras só são separadas por um espaço.
+
+function contarPalavras(frase) {
+  const palavras = frase.split(' ')
+  return palavras.length
+}
+
+console.log(contarPalavras("Sou uma frase")); // retornará 3
+console.log(contarPalavras("Me divirto aprendendo a programar")); // retornará 5
