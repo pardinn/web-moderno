7b7edac7bcc253bbc10652c625a57743cea35780
Victor Moraes <dt.victor@gmail.com>
2021-01-01 16:44:28 -0300

    Revisao ESNext

---
+++ esnext/revisao2.js
@@ -0,0 +1,26 @@
+// Arrow Function
+const soma = (a, b) => a + b;
+console.log(soma(2, 3));
+
+// Arrow Function (this)
+const lexico1 = () => console.log(this === exports);
+const lexico2 = lexico1.bind({});
+lexico1();
+lexico2();
+
+function log(texto = 'Node') {
+  console.log(texto);
+}
+log(); //imprime default
+log(undefined); //imprime default
+log(null); //imprime null
+log('Sou mais forte'); //imprime parametro
+
+// operador rest
+function total(...numeros) {
+  let total = 0;
+  numeros.forEach(n => total += n);
+  return total;
+}
+console.log(total(2, 3, 4, 5));
+
