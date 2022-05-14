8f06a34c9455a3e0e360f10b56ceb4dc2d5d01d7
Victor Moraes <vrme@gft.com>
2022-05-14 09:03:12 -0300

    Fazendo upload de arquivos

--- ajax/server.js
+++ ajax/server.js
@@ -1,9 +1,31 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const multer = require('multer');
 
 app.use(express.static('.'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload');
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+});
+
+const upload = multer({ storage }).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
 
 app.listen(8080, () => console.log('Executando...'));
