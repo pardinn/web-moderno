8744f6a2841c320adc5236c8b0f7dae60fa70603
Victor Moraes <vrme@gft.com>
2022-05-30 19:33:53 -0300

    Projeto SPA: Task Deps

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-function depsCSS(callback) {
-    return callback()
+function depsCSS() {
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(uglifycss({ "uglyComments": false }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function depsFonts(callback) {
-    return callback()
+function depsFonts() {
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
