e007769491a4e758a5ad50ed9178510952ca6d16
Victor Moraes <dt.victor@gmail.com>
2022-06-25 17:05:27 -0300

    Adicionando ESLint

--- fundamentos/templateString.js
+++ fundamentos/templateString.js
@@ -1,14 +1,14 @@
-const nome = 'Rebeca'
-const concatenacao = 'Olá ' + nome + '!'
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + '!';
 
 const template = `
     Olá
-    ${nome}!`
+    ${nome}!`;
 
-console.log(concatenacao,template)
+console.log(concatenacao, template);
 
 //expressões
-console.log(`1 + 1 = ${1 + 1}`)
+console.log(`1 + 1 = ${1 + 1}`);
 
-const up = texto => texto.toUpperCase()
-console.log(`Ei... ${up('cuidado')}!`)
+const up = (texto) => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!`);
