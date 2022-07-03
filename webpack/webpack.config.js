f90c8d9ecbb4f969eb258be79f65d06d112ad44f
Victor Moraes <dt.victor@gmail.com>
2022-07-03 10:15:20 -0300

    Webpack: Carregando o arquivo .CSS

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     filename: 'principal.js',
     path: `${__dirname}/public`,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', // Adiciona CSS à DOM injetando a tag <style>
+          'css-loader', // interpreta @import, url()...
+        ],
+      },
+    ],
+  },
 };
